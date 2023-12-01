23a4a2c3ca7c5e4f073b9a34955e1dfc0d7c59d2
GBalatti9 <gas.balatti@gmail.com>
2023-12-01 11:07:54 -0300

    New approach: now the app is serverless i have comm ented all the express code and also the useFetch hook as im not working anymore with api consumt ion it is just a function that scrapps and return a json file with the necessary information. It  is working like an api should work but i had trouble deploying the app with that code so im try ing another approach, however i leave the old code written and commented

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
 const { scrappWeb } = require('./helpers/webScrapping');
 
-const app = express();
+const scrapping = async () => {
+    await scrappWeb();
+}
 
-app.use(cors());
-app.use(express.static( path.join ( __dirname, '../frontend/dist' ) ));
+setInterval(async () => {
+    await scrapping();
+}, 60000);
 
-const port = 3000;
+// scrapping();
 
-app.get('/', async (req, res) => {
+// const app = express();
 
-    res.send("App working correctly...")
-    // try {        
-    //     const data  = await scrappWeb();
-    //     res.json({ data });
-    // } catch (error) {
-    //     console.log(error);
-    // }
-})
+// app.use(cors());
+// app.use(express.static( path.join ( __dirname, '../frontend/dist' ) ));
 
-app.get('/web-scrapping-api', async (req, res) => {
-    const data  = await scrappWeb();
-    res.json({ data });
-})
+// const port = 3000;
 
-app.listen( process.env.PORT || port, () => {
-    console.log(`App listening on ${port}...`);
-})
+// app.get('/', async (req, res) => {
+
+//     res.send("App working correctly...")
+//     // try {        
+//     //     const data  = await scrappWeb();
+//     //     res.json({ data });
+//     // } catch (error) {
+//     //     console.log(error);
+//     // }
+// })
+
+// app.get('/web-scrapping-api', async (req, res) => {
+//     const data  = await scrappWeb();
+//     res.json({ data });
+// })
+
+// app.listen( process.env.PORT || port, () => {
+//     console.log(`App listening on ${port}...`);
+// })
--- helpers/webScrapping.js
+++ helpers/webScrapping.js
@@ -1,16 +1,17 @@
 const { chromium } = require('playwright');
+const path = require('path');
 const fs = require('fs');
 
 const scrappWeb = async () => {
     let browser;
 
     try {
+        console.log("Opening browser...");
         browser = await chromium.launch({ headless: true });
         const page = await browser.newPage();
-        
+
         await page.goto('https://open.bymadata.com.ar/#/dashboard', { timeout: 120000 });
         const tableSelector = '.mat-table';
-        
 
         await page.waitForSelector(tableSelector);
 
@@ -25,11 +26,18 @@ const scrappWeb = async () => {
 
         console.log('Extracting table data...');
         const data = await extractTableData(tableSelector);
-        return data;
+
+        const filePath = path.join(__dirname, '../../frontend', 'src', 'data', 'scrapedData.json');
+        console.log({ filePath });
+        await fs.promises.writeFile(filePath, JSON.stringify( data ));
+        await fs.promises.writeFile('data/scrapedData.json', JSON.stringify(data));
+        console.log('Data written to file successfully.')
+        // return data;
     } catch (error) {
         console.log(error);
     } finally {
         if (browser) {
+            // await page.close();
             await browser.close();
         }
     }
