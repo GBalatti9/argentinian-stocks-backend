a0c2e07116099c8aa8a8c6a4dfcc95fa66f2cf3f
GBalatti9 <gas.balatti@gmail.com>
2023-12-26 13:02:07 -0300

    Headless true

--- helpers/puppeteer.js
+++ helpers/puppeteer.js
@@ -5,6 +5,7 @@ const webScrapping = async () => {
 
     try {
         const browser = await puppeteer.launch({
+            headless: true,
             args: [
                 "--disable-setuid=sandbox",
                 "--no-sandbox",
@@ -16,8 +17,8 @@ const webScrapping = async () => {
         const page = await browser.newPage();
         console.log('Open browser...');
 
-        // const navigationTimeout = 300000;
-        // page.setDefaultNavigationTimeout(navigationTimeout);
+        const navigationTimeout = 300000;
+        page.setDefaultNavigationTimeout(navigationTimeout);
         console.log('Navigation to the page...');
 
         await page.goto('https://open.bymadata.com.ar/#/dashboard');
