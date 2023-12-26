6a92ed3245f21f252c63a082e4cf2cfa17a98cd2
GBalatti9 <gas.balatti@gmail.com>
2023-12-26 12:25:22 -0300

    Updated args and executable path for Puppeteer

--- helpers/puppeteer.js
+++ helpers/puppeteer.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 const webScrapping = async () => {
 
     try {
         const browser = await puppeteer.launch({
-            headless: 'new',
-            slowMo: 400
+            args: [
+                "--disable-setuid=sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+            executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
         });
         const page = await browser.newPage();
         console.log('Open browser...');
